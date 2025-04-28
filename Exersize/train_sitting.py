ef18e68ab0f1c46b845ac317791c59f2dfc56295
Darshit Hirani <darshithirani007@gmail.com>
2025-04-28 10:52:46 +0530

    added new files

--- Exersize/train_sitting.py
+++ Exersize/train_sitting.py
@@ -1,1 +1,7 @@
-print("hello world")
+# def find_opposite_seat_and_type(seat_number):
+#     if not 1 <= seat_number <= 108:
+#         return "Invalid seat number. Please enter a number between 1 and 108."
+
+# seat_number = int(input("Enter seat number (1-108): "))
+
+# ---------------------------------------------------------------------------------------
